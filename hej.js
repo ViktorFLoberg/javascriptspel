596575e86f1a0fb30365f8fe9a0d6f1fdc8b3198
ViktorFLoberg <116791927+ViktorFLoberg@users.noreply.github.com>
2023-05-19 16:54:26 +0200

    Update hej.js

--- hej.js
+++ hej.js
@@ -9,11 +9,16 @@ function shuffleGrid() {
     gridItems[randomIndex].style.order = i;
   }
 }
+
+function checkGameWon() {
+  return gridItems.length === matchedItems.length;
+}
+
 function checkMatch() {
   if (selectedItems.length === 2) {
-    const firstItem = parseInt( selectedItems[0].dataset.value);
-    const secondItem = parseInt( selectedItems[1].dataset.value);
-    if (firstItem + secondItem===15) {
+    const firstItem = parseInt(selectedItems[0].dataset.value);
+    const secondItem = parseInt(selectedItems[1].dataset.value);
+    if (firstItem + secondItem === 15) {
       selectedItems.forEach(item => item.classList.add('matched'));
       matchedItems.push(...selectedItems);
     } else {
@@ -21,23 +26,26 @@ function checkMatch() {
     }
     selectedItems = [];
     clickCount = 0;
+
+    if (checkGameWon()) {
+      showGameWonPopup();
+    }
   }
 }
+
 function flipItem(item) {
   item.classList.toggle('flipped');
 }
 
-
 flipItem(gridItems[0]);
-
 
 function handleClick() {
   if (matchedItems.includes(this)) return;
   if (selectedItems.includes(this)) return;
-  if (clickCount <= 2){
+  if (clickCount <= 2) {
     this.classList.add('selected');
     flipItem(this);
-    selectedItems.push(this);  
+    selectedItems.push(this);
     clickCount++;
   }
   checkMatch();
@@ -48,14 +56,18 @@ function resetGame() {
     item.classList.remove('selected', 'matched');
     item.style.order = '';
   });
-  
+
   matchedItems = [];
   selectedItems = [];
   shuffleGrid();
 }
 
+function showGameWonPopup() {
+  const popup = window.open('', 'Vinst!', 'width=400,height=200');
+  popup.document.write('<h1>Grattis!</h1>');
+  popup.document.write('<p>Du vann!</p>');
+}
 
 gridItems.forEach(item => item.addEventListener('click', handleClick));
-
 
 shuffleGrid();
